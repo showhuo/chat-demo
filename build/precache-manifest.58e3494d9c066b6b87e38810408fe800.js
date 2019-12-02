b7a8c03432fa6a3a07e7aeaab7a11b40e997db8c
onlyc <onlyc>
2019-12-02 09:06:47 +0800

    尝试部署到github pages

---
+++ build/precache-manifest.58e3494d9c066b6b87e38810408fe800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec61e8f6a4323d2342ef37a4b2948c6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e8d323790467a4b2eb7",
+    "url": "/static/css/2.74523050.chunk.css"
+  },
+  {
+    "revision": "f48ee893cb4d1d466d09",
+    "url": "/static/css/main.67868691.chunk.css"
+  },
+  {
+    "revision": "7e8d323790467a4b2eb7",
+    "url": "/static/js/2.53ed7806.chunk.js"
+  },
+  {
+    "revision": "f48ee893cb4d1d466d09",
+    "url": "/static/js/main.43c01ae0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
